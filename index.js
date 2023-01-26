01f6a93f709383f27915f5832b234d72f776315e
erdenetsogt <a.erdenetsogt@gmail.com>
2023-01-26 15:13:04 +0800

    categories findAll, findOne, create

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 const express = require("express");
 const cors = require("cors");
+const { v4 } = require("uuid");
 
 const port = 3000;
 const app = express();
 
-const users = [];
-
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello");
+const categories = [
+    {
+        id: v4(),
+        name: "Politics",
+    },
+    {
+        id: v4(),
+        name: "Sport",
+    },
+];
+
+app.get("/categories", (req, res) => {
+    res.json(categories);
 });
 
-app.get("/users", (req, res) => {
-    res.json(users);
+app.get("/categories/:id", (req, res) => {
+    const { id } = req.params;
+    const one = categories.find((category) => category.id === id);
+    if (one) {
+        res.json(one);
+    } else {
+        res.sendStatus(404);
+    }
 });
 
-app.get("/users/save", (req, res) => {
-    users.push({ name: "Bat" });
-    res.json(users);
+app.post("/categories", (req, res) => {
+    const { name } = req.body;
+    categories.push({ id: v4(), name: name });
+    res.sendStatus(201);
 });
 
 app.listen(port, () => {
