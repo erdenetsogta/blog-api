d69902f58a67116015f544f710be5385e3084376
erdenetsogt <a.erdenetsogt@gmail.com>
2023-01-30 14:46:00 +0800

    fs module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 const { v4: uuid } = require("uuid");
 
 const port = 8000;
@@ -8,23 +9,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let categories = [
-    {
-        id: uuid(),
-        name: "Politics",
-    },
-    {
-        id: uuid(),
-        name: "Sport",
-    },
-];
+function readCategories() {
+    const content = fs.readFileSync("categories.json");
+    const categories = JSON.parse(content);
+    return categories;
+}
 
 app.get("/categories", (req, res) => {
+    const categories = readCategories();
     res.json(categories);
 });
 
 app.get("/categories/:id", (req, res) => {
     const { id } = req.params;
+    const categories = readCategories();
     const one = categories.find((category) => category.id === id);
     if (one) {
         res.json(one);
@@ -36,16 +34,22 @@ app.get("/categories/:id", (req, res) => {
 app.post("/categories", (req, res) => {
     const { name } = req.body;
     const newCategory = { id: uuid(), name: name };
+
+    const categories = readCategories();
+
     categories.push(newCategory);
+    fs.writeFileSync("categories.json", JSON.stringify(categories));
+
     res.sendStatus(201);
 });
 
 app.delete("/categories/:id", (req, res) => {
     const { id } = req.params;
+    const categories = readCategories();
     const one = categories.find((category) => category.id === id);
     if (one) {
         const newList = categories.filter((category) => category.id !== id);
-        categories = newList;
+        fs.writeFileSync("categories.json", JSON.stringify(newList));
         res.json({ deletedId: id });
     } else {
         res.sendStatus(404);
@@ -55,13 +59,39 @@ app.delete("/categories/:id", (req, res) => {
 app.put("/categories/:id", (req, res) => {
     const { id } = req.params;
     const { name } = req.body;
+    const categories = readCategories();
     const index = categories.findIndex((category) => category.id === id);
     if (index > -1) {
         categories[index].name = name;
+        fs.writeFileSync("categories.json", JSON.stringify(categories));
         res.json({ updatedId: id });
     } else {
         res.sendStatus(404);
     }
+});
+
+app.get("/users/save", (req, res) => {
+    const newUser = [
+        {
+            name: "Sarnai",
+            id: 1,
+        },
+    ];
+    fs.writeFileSync("data.json", JSON.stringify(newUser));
+    res.json(["success"]);
+});
+
+app.get("/users/read", (req, res) => {
+    const content = fs.readFileSync("data.json");
+    res.json(JSON.parse(content));
+});
+
+app.get("/users/update", (req, res) => {
+    const content = fs.readFileSync("data.json");
+    const users = JSON.parse(content);
+    users.push({ id: 2, name: "Bold" });
+    fs.writeFileSync("data.json", JSON.stringify(users));
+    res.json({});
 });
 
 app.listen(port, () => {
